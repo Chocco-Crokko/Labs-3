30dcfdbcd46b83208d9a3ce33224ad97a6349bcc
Denis <zastiralkin@gmail.com>
2016-10-06 18:27:37 +0400

    one more fix

--- lab-1/test/main.cpp
+++ lab-1/test/main.cpp
@@ -1,6 +1,15 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "../include/print_hello_world.h"
+#include <string>
+
+bool is_palindrome(const std::string &str)
+{
+	for (auto beg = str.begin(), end = str.end() - 1; beg < end; ++beg, --end)
+		if (*beg != *end) 
+			return false;
+	return true;
+}
 
 TEST_CASE( "Palindrome is checked", "[palindrome]" ) {
     REQUIRE( is_palindrome("abcba") == true );
